6a354e647f454e5709c4c5020011e6bf826b4663
Vilas Kumar Chitrakaran <cvilas@gmail.com>
2018-12-27 10:45:10 +0000

    updated to latest clang tools

--- cmake/modules/FindClangTools.cmake
+++ cmake/modules/FindClangTools.cmake
@@ -31,9 +31,9 @@
 #  CLANG_TIDY_FOUND, Whether clang format was found
 
 find_program(CLANG_TIDY_BIN
-  NAMES clang-tidy-7 
+  NAMES clang-tidy-8
+  clang-tidy-7
   clang-tidy-6.0
-  clang-tidy-5.0
   clang-tidy
   PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin
         NO_DEFAULT_PATH
@@ -48,9 +48,9 @@ else()
 endif()
 
 find_program(CLANG_FORMAT_BIN
-  NAMES clang-format-7
+  NAMES clang-format-8
+  clang-format-7
   clang-format-6.0
-  clang-format-5.0
   clang-format
   PATHS ${ClangTools_PATH} $ENV{CLANG_TOOLS_PATH} /usr/local/bin /usr/bin
         NO_DEFAULT_PATH
